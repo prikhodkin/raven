36af2b38de8b71fa58dde6f9f04792838340ee7d
prikhodkin <info@prikhodkin.com>
2020-07-29 01:15:33 +0500

    Добавляет адаптивную шапку

--- src/blocks/search/search.js
+++ src/blocks/search/search.js
@@ -1,4 +1,4 @@
-modules.define('example', ['i-bem-dom'], function(provide, bemDom) {
+modules.define('search', ['i-bem-dom'], function(provide, bemDom) {
 
 provide(bemDom.declBlock(this.name, {
     onSetMod: {
