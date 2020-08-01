4f4f76c76b1eda52a69576ea1b03b01eb04450d8
prikhodkin <info@prikhodkin.com>
2020-08-02 01:37:31 +0500

    Добавляет страницы новостей

---
+++ src/blocks/news-preview/news-preview.js
@@ -0,0 +1,13 @@
+modules.define('news-preview', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
