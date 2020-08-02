3e8a0e367f9f995a3bc4bddd5dbca44e8a60050d
prikhodkin <info@prikhodkin.com>
2020-08-03 01:12:09 +0500

    Updates

--- js/general.js
+++ js/general.js
@@ -29,6 +29,20 @@ $(".product__control--min").click(function () {
   $input.change();
   return false;
 });
+$(".cart___control--max").click(function () {
+  var $input = $(this).parent().find("input");
+  $input.val(parseInt($input.val()) + 1);
+  $input.change();
+  return false;
+});
+$(".cart___control--min").click(function () {
+  var $input = $(this).parent().find("input");
+  var count = parseInt($input.val()) - 1;
+  count = count < 1 ? 1 : count;
+  $input.val(count);
+  $input.change();
+  return false;
+});
 $(".product__control--max").click(function () {
   var $input = $(this).parent().find("input");
   $input.val(parseInt($input.val()) + 1);
