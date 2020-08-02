f45155b9ca17d78918e05cd36bff62eaba309fe6
prikhodkin <info@prikhodkin.com>
2020-08-02 15:38:00 +0500

    Добавляет страницы товара, контакты

--- src/js/general.js
+++ src/js/general.js
@@ -24,6 +24,23 @@ $(`.product-card__control--max`).click(function () {
   return false;
 });
 
+
+$(`.product__control--min`).click(function () {
+  const $input = $(this).parent().find(`input`);
+  let count = parseInt($input.val()) - 1;
+  count = count < 1 ? 1 : count;
+  $input.val(count);
+  $input.change();
+  return false;
+});
+
+$(`.product__control--max`).click(function () {
+  const $input = $(this).parent().find(`input`);
+  $input.val(parseInt($input.val()) + 1);
+  $input.change();
+  return false;
+});
+
 $(`.slider__list`).slick({
   prevArrow: `.slider__control--prev`,
   nextArrow: `.slider__control--next`,
@@ -53,14 +70,15 @@ if($(window).width() >= 768) {
 
 
 
-$('.gallery__list').slick({
+$('.text__gallery .gallery__list').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
   dots: false,
   fade: false,
   infinite: true,
-  asNavFor: '.gallery__sub-list',
+  adaptiveHeight: true,
+  asNavFor: '.text__gallery .gallery__sub-list',
   responsive: [
     {
       breakpoint: 1199,
@@ -71,15 +89,72 @@ $('.gallery__list').slick({
   ]
 });
 
-$('.gallery__sub-list').slick({
+$('.text__gallery .gallery__sub-list').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
-  asNavFor: '.gallery__list',
+  asNavFor: '.text__gallery .gallery__list',
   dots: false,
   centerMode: false,
   infinite: true,
   focusOnSelect: true,
   arrows: true,
-  prevArrow: `.gallery__control--prev`,
-  nextArrow: `.gallery__control--next`,
+  prevArrow: `.text__gallery .gallery__control--prev`,
+  nextArrow: `.text__gallery .gallery__control--next`,
 });
+
+$('.product__gallery .gallery__list').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  fade: false,
+  infinite: true,
+  adaptiveHeight: true,
+  asNavFor: '.product__gallery .gallery__sub-list',
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        dots: true
+      }
+    },
+  ]
+});
+
+$('.product__gallery .gallery__sub-list').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.product__gallery .gallery__list',
+  dots: false,
+  centerMode: false,
+  infinite: true,
+  focusOnSelect: true,
+  arrows: true,
+  prevArrow: `.product__gallery .gallery__control--prev`,
+  nextArrow: `.product__gallery .gallery__control--next`,
+});
+
+
+const tabs = document.querySelectorAll(`[data-tab]`);
+const tabsContent = document.querySelectorAll(`[data-content]`);
+
+const showTab = function (arrTab, arrContent, dataTab, dataContent) {
+  arrTab.forEach(function (item) {
+    item.addEventListener(`click`, function () {
+      for (let index = 0; index < arrTab.length; index++) {
+        arrTab[index].classList.remove(`tabs__control--active`);
+      }
+      item.classList.add(`tabs__control--active`);
+      const tabID = item.getAttribute(dataTab);
+      for (let o = 0; o < arrContent.length; o++) {
+        arrContent[o].classList.remove(`tabs__content--show`);
+        const contentID = arrContent[o].getAttribute(dataContent);
+        if (tabID === contentID) {
+          arrContent[o].classList.add(`tabs__content--show`);
+        }
+      }
+    });
+  });
+};
+
+showTab(tabs, tabsContent, `data-tab`, `data-content`);
